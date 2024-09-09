d67c5e302cb18a9b31f8fabdf8c88756a2cfacf5
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2024-09-09 12:32:42 +0300

    Update instr_mem.v

--- instr_mem.v
+++ instr_mem.v
@@ -7,16 +7,16 @@ output [31:0]RD;
   
 reg[31:0]MEM[1023:0];
 
-assign RD =(rst==1)?0:MEM[A[31:2]] ;
-  initial begin
+assign RD =(~rst)?0:MEM[A[31:2]] ;
+initial begin
 $readmemh("MEM_FILE.hex",MEM);
 end
-// initial begin
-// 	 MEM[0]=32'h0064a423;
-// 	 MEM[1]=32'h00c58000;
-// 	MEM[2]=32'h0062E233;
-// 	MEM[3]=32'hFFC4A303;
+//initial begin
+//	 MEM[0]=32'h0064a423;
+//	 MEM[1]=32'h00c58000;
+	//MEM[2]=32'h0062E233;
+	//MEM[3]=32'hFFC4A303;
 
-// end
+//end
 
 endmodule
