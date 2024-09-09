372590f298586d6df8ee1c513e924878be376599
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2024-09-09 12:32:00 +0300

    Update DATA_MEM.v

--- DATA_MEM.v
+++ DATA_MEM.v
@@ -3,9 +3,11 @@ module DATA_MEM(A,WD,clk,WE,RD,rst);
 input [31:0]A,WD;
 input clk,WE,rst;
 output [31:0]RD;
+
 reg[31:0]MEM[1023:0];
 
-assign RD =(WE==0)?MEM[A]:0 ;
+assign RD =(WE==0&&rst)?MEM[A]:0 ;
+
 always @(posedge clk ) begin
      if (WE) begin
     MEM[A]<=WD;
